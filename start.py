916fad43819fff1ddbae0c9325a3301edebdf99a
Martin Zhu <martin_zhu@brown.edu>
2016-05-03 00:19:43 -0400

    updated website

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template("start.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
