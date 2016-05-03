ef6a09c85808b1f557b311939703bd571f6bea59
Martin Zhu <martin_zhu@brown.edu>
2016-05-03 16:31:50 -0400

    updated some stuff

--- static/js/script.js
+++ static/js/script.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 
 	$('#samplebox').click(function() {
     	if($("#samplebox").is(':checked')){
+    		$("#main_stuff").hide()
     		$("#sample_stuff").show()
     	}
 		else {
     		$("#sample_stuff").hide()
+    		$("#main_stuff").show()
 		}
 	});
 
