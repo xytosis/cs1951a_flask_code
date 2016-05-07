151b7e2af4cb24affc2b9812b7cae18fb5e90bbd
Martin Zhu <martin_zhu@brown.edu>
2016-05-07 18:51:28 -0400

    added parallelization for words by time

--- start.py
+++ start.py
@@ -5,9 +5,15 @@ import urllib2
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from karma_predictor import model, vectorizer, top_words
+import threading
+import Queue
 
 application = Flask(__name__)
 SOLR_IP = "54.173.242.173:8983"
+
+def get_url(q, req, start):
+    response = json.loads(urllib2.urlopen(req).read())
+    q.put([start, response["response"]["numFound"]])
 
 @application.route("/")
 def hello():
@@ -25,16 +31,34 @@ def freq_by_time():
         ranges.append((cur_date, cur_date + relativedelta(months=1)))
         cur_date = cur_date + relativedelta(months=1)
     time_to_count = []
+    threads = []
+    q1 = []
+    q2 = []
     for r in ranges:
         start = r[0].strftime("%Y-%m-%dT%H:%M:%SZ")
         end = r[1].strftime("%Y-%m-%dT%H:%M:%SZ")
         req = "http://" + SOLR_IP + "/solr/comments/select?q=body:\"" + text + "\"&rows=0&wt=json&fq=created_utc:[" + start + "%20TO%20" + end + "]"
         req2 = "http://" + SOLR_IP + "/solr/comments/select?rows=0&wt=json&q=created_utc:[" + start + "%20TO%20" + end + "]"
-        response = json.loads(urllib2.urlopen(req).read())
-        response2 = json.loads(urllib2.urlopen(req2).read())
-        time_to_count.append([start, response["response"]["numFound"], response2["response"]["numFound"]])
+        t = threading.Thread(target=get_url, args=(q1, req, start))
+        t2 = threading.Thread(target=get_url, args=(q2, req2, start))
+        threads.append(t)
+        threads.append(t2)
+        t.start()
+        t2.start()
+        #time_to_count.append([start, response["response"]["numFound"], response2["response"]["numFound"]])
 
-    return json.dumps(time_to_count)
+    for t in threads:
+        t.join()
+
+    start_to_stuff = dict()
+    for q in q1:
+        if q[0] in start_to_stuff:
+            start_to_stuff[q[0]].append(q[1])
+        else:
+            start_to_stuff[q[0]] = q[1]
+
+    print start_to_stuff
+    return ""
 
 
 # queries solr and returns frequency to subreddit
